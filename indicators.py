a5209bc49a15d5457ec5c21b6b6e56c25e9efb5b
kr77the-source <kr77the@gmail.com>
2026-08-04 02:09:40 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,7 +1,7 @@
 # ==========================================
 # Institutional Trade Engine
 # File : indicators.py
-# Version : 1.0
+# Version : 2.0
 # ==========================================
 
 import yfinance as yf
@@ -12,21 +12,14 @@ def get_vwap(symbol):
     try:
 
         df = yf.download(
-
             symbol,
-
             period="1d",
-
             interval="5m",
-
             progress=False,
-
             auto_adjust=False
-
         )
 
         if df.empty:
-
             return None
 
         tp = (df["High"] + df["Low"] + df["Close"]) / 3
@@ -37,8 +30,38 @@ def get_vwap(symbol):
 
         return round(float(vwap.iloc[-1]), 2)
 
-    except Exception as e:
+    except Exception:
 
-        print("VWAP Error :", e)
+        return None
+
+
+def get_ema(symbol):
+
+    try:
+
+        df = yf.download(
+            symbol,
+            period="5d",
+            interval="5m",
+            progress=False,
+            auto_adjust=False
+        )
+
+        if len(df) < 60:
+            return None
+
+        ema20 = df["Close"].ewm(span=20).mean()
+
+        ema50 = df["Close"].ewm(span=50).mean()
+
+        return {
+
+            "ema20": round(float(ema20.iloc[-1]),2),
+
+            "ema50": round(float(ema50.iloc[-1]),2)
+
+        }
+
+    except:
 
         return None
